31f64ea6127d0f914917b58b865bea4afab15ba5
Trobots <wlhsroboticsteam@gmail.com>
2024-02-14 14:05:39 -0500

    jas the pose.plus() and pose.div() methods were not giving a good answer... brute forced it.

--- src/main/java/frc/robot/subsystems/WL_PhotonCameraHelper.java
+++ src/main/java/frc/robot/subsystems/WL_PhotonCameraHelper.java
@@ -22,6 +22,7 @@ public class WL_PhotonCameraHelper extends SubsystemBase{
     public CameraWithTags tagsVisible = CameraWithTags.NoCamerasWithTags;
     private Pose3d AveragePose3d = new Pose3d (0.0, 0.0, 0.0, new Rotation3d (0.0, 0.0, 0.0));
 
+    private double sumx, sumy, sumz, sumYaw, sumPitch, sumRoll = 0;
     private String AveragePoseString = "";
     public WL_PhotonCameraHelper () {
 
@@ -35,29 +36,51 @@ public class WL_PhotonCameraHelper extends SubsystemBase{
     }
 
 
+    private void zeroSums () {
+        sumx = 0.0;
+        sumy = 0.0;
+        sumz = 0.0;
+        sumPitch = 0.0;
+        sumRoll = 0.0;
+        sumYaw = 0.0;
+    }
+
+    private Pose3d calcAveragePose3d(double poses){
+
+        return new Pose3d(sumx / poses, sumy / poses, sumz/poses,
+               new Rotation3d(sumRoll/poses, sumPitch/poses, sumYaw/poses ));
+
+    }
+
     public void periodic () {
         CameraWithTags localCameraTags = CameraWithTags.NoCamerasWithTags;
 
+        zeroSums();
         int cameras_withTags = 0;
         double sumLatencyMillis = 0;
-        Pose3d sumPose = new Pose3d(new Translation3d(0,0,0), new Rotation3d(0,0,0));
+        // robot axis labled in these web pages
+        // https://docs.wpilib.org/en/stable/docs/software/basic-programming/coordinate-system.html
+        // https://docs.photonvision.org/en/latest/docs/apriltag-pipelines/coordinate-systems.html
+
         for (Map.Entry<String, WL_PhotonCamera> e : cameras.entrySet()){
             if (e.getValue().hasTargets()) {
                 localCameraTags = CameraWithTags.SomeCamerasWithTags;
-
+                sumx = e.getValue().getRobotPose3d().getX();
+                sumy = e.getValue().getRobotPose3d().getY();
+                sumz = e.getValue().getRobotPose3d().getZ();
+                sumRoll = e.getValue().getRobotPose3d().getRotation().getX();
+                sumPitch = e.getValue().getRobotPose3d().getRotation().getY();
+                sumYaw = e.getValue().getRobotPose3d().getRotation().getZ();
                 cameras_withTags++;
-                sumPose = sumPose.plus(new Transform3d (e.getValue().getRobotPose3d().getTranslation(),
-                                                        e.getValue().getRobotPose3d().getRotation()));
                 sumLatencyMillis = sumLatencyMillis + e.getValue().getLatencyMillis();
-
             }
             else {
-                // Currently not sure what to do here.
+                // Currently not sure what to do here.  No tags visible on this camera
             }
         }
 
         if (cameras_withTags > 0) {
-            AveragePose3d = sumPose.div(cameras_withTags);
+            AveragePose3d = calcAveragePose3d(cameras_withTags);
             RobotContainer.getInstance().m_robotDrive.addVision(AveragePose3d.toPose2d(),(sumLatencyMillis / cameras_withTags) );
             AveragePoseString = String.format ("Cams: %s X: %.2f Y: %.2f Yaw: %.0f",
                  cameras_withTags, AveragePose3d.getX(), AveragePose3d.getY(), AveragePose3d.getRotation().getZ() );
@@ -67,9 +90,7 @@ public class WL_PhotonCameraHelper extends SubsystemBase{
             AveragePoseString = "No Tags Visible";
 
         }
-
         tagsVisible = localCameraTags;
-
     }
 
     public String getCamString(String camName){
