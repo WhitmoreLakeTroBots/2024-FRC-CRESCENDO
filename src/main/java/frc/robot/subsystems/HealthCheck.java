46008a3894f830a2cdcf3f688ab4c82756a8c105
Wingnut813 <97901544+Wingnut813@users.noreply.github.com>
2024-03-11 19:27:00 -0400

    updates to health check

--- src/main/java/frc/robot/subsystems/HealthCheck.java
+++ src/main/java/frc/robot/subsystems/HealthCheck.java
@@ -5,11 +5,13 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotContainer;
 import frc.robot.commands.intakeCommands.intakeCmd;
 import frc.robot.commands.intakeCommands.pivotCmd;
+import frc.utils.CommonLogic;
 import frc.utils.cmdDelay;
 
 import java.util.Map;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
 
 import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -19,33 +21,36 @@ import edu.wpi.first.wpilibj.DigitalInput;
  */
 public class HealthCheck extends SubsystemBase {
 
-   // public DigitalInput IntakeSen;
+    // public DigitalInput IntakeSen;
 
-    private int faultCount = 0; 
-    private static double TempTreshold = 75;  //temp in Celsius
+    private int faultCount = 0;
+    private static double TempTreshold = 75; // temp in Celsius
 
-    private Map <String, CANSparkMax> RobotSparks;
+    private Map<String, CANSparkMax> RobotSparks;
+
     /**
     *
     */
     public HealthCheck() {
 
-        //addChild("Digital Input 1" , BeamBreak1);
+        // addChild("Digital Input 1" , BeamBreak1);
 
     }
 
-        
-    public void registerSparkMAx(String SparkName, CANSparkMax newSpark){
-    //make method that allows us to register each SparkMax so that we can loop through for health checks
-    RobotSparks.put(SparkName, newSpark);
-    
+    public void registerSparkMAx(String SparkName, CANSparkMax newSpark, boolean inverted, int stallLimit,
+            int rollLimit, IdleMode breakMode) {
+        // make method that allows us to register each SparkMax so that we can loop
+        // through for health checks
+        RobotSparks.put(SparkName, newSpark);
+        CommonLogic.setSparkParamsBase(newSpark, inverted, stallLimit, rollLimit, breakMode);
+
     }
 
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
-       // checkIntake();
-       CAMHealthCheck();
+        // checkIntake();
+        CAMHealthCheck();
     }
 
     @Override
@@ -57,42 +62,39 @@ public class HealthCheck extends SubsystemBase {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
-private void CAMHealthCheck(){
-    //make list of health checks for each CAN / SPARK MAX to run 
+    private void CAMHealthCheck() {
+        // make list of health checks for each CAN / SPARK MAX to run
+        if (!RobotSparks.isEmpty()) {
+            for (Map.Entry<String, CANSparkMax> e : RobotSparks.entrySet()) {
+                // run tests
 
-    for(Map.Entry<String, CANSparkMax> e : RobotSparks.entrySet()){
-        //run tests
+                tempCheck(e.getValue().getMotorTemperature(), e.getKey());
+                // other checks
 
-        tempCheck(e.getValue().getMotorTemperature(), e.getKey());
-        // other checks 
-
+            }
         }
     }
 
-
-    
-
-
-private void tempCheck(double Temp, String name){
-    if (Temp >= TempTreshold){
-        //send alert
-        writeError("Motor" + name  +" has temp of " + Temp 
-            + " which exceedes Temp Threashold of " + TempTreshold  );
+    private void tempCheck(double Temp, String name) {
+        if (Temp >= TempTreshold) {
+            // send alert
+            writeError("Motor" + name + " has temp of " + Temp
+                    + " which exceedes Temp Threashold of " + TempTreshold);
+        }
     }
-}
 
-private void writeError(String nMsg){
+    private void writeError(String nMsg) {
 
-    System.err.println(nMsg);
-    faultCount++; 
-}
+        System.err.println(nMsg);
+        faultCount++;
+    }
 
-public int getFaultCount(){
-    return faultCount;
-}
+    public int getFaultCount() {
+        return faultCount;
+    }
 
-public void resetFaultCount(){
-    faultCount = 0;
-}
+    public void resetFaultCount() {
+        faultCount = 0;
+    }
 
 }
