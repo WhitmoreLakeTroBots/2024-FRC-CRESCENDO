f0b1baa993e0a421c36d045675a426196547145b
Jeff <xjschwen@gmail.com>
2024-08-23 13:39:40 -0400

    jas quick powershell and groovy work

--- buildSrc/src/main/groovy/org/wlrobotics/common/ComputerChecker.groovy
+++ buildSrc/src/main/groovy/org/wlrobotics/common/ComputerChecker.groovy
@@ -7,13 +7,23 @@ import java.net.InetAddress
 
         try {
             returnValue = InetAddress.getByName(computerName).isReachable(1000) // 1000ms timeout
+
         } catch (Exception e) {
             returnValue = false
         }
+
+
         return returnValue
     }
 
+    String getHostAddress (computerName) {
+
+        return InetAddress.getByName(computerName).getHostAddress().toString()
+
+    }
+
     static void main(String[] args) {
+
         if (args.length != 1) {
             println "Usage: groovy ComputerChecker.groovy <computer_name>"
             return
